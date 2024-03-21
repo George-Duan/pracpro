9f528869fc708c7682f30fc5a74211f6528d7946
dlixiong <george.duanshier@gmail.com>
2024-03-21 16:19:34 +0800

    设计模式的艺术中的装饰例子

---
+++ design-patterns/src/main/java/com/leshion/decorator/Component.java
@@ -0,0 +1,5 @@
+package com.leshion.decorator;
+
+public abstract class Component {
+    public abstract void display();
+}
---
+++ design-patterns/src/main/java/com/leshion/decorator/Window.java
@@ -0,0 +1,8 @@
+package com.leshion.decorator;
+
+public class Window extends Component{
+    @Override
+    public void display() {
+        System.out.println("显示窗体");
+    }
+}
